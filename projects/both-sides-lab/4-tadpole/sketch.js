a7e72b592267646fec46f63d0a0e070edfd4c456
Nan Zhao <nanzhao@nyu.edu>
2018-11-08 23:29:36 +0800

    Refine the tadpole project

--- projects/both-sides-lab/4-tadpole/sketch.js
+++ projects/both-sides-lab/4-tadpole/sketch.js
@@ -5,6 +5,8 @@ var t;
 var C_GRAVITY = 20;
 var mumPos;
 var tadpoles = [];
+var thisTime;
+
 function setup() {
   createCanvas(200, 200);
   background(255);
@@ -13,26 +15,38 @@ function setup() {
   fill(0);
   mumPos = createVector(width / 2, height / 2);
   t = new Tadpole(random(width), random(height), childR);
-  
-  for (var i = 0;i<20; i++){
+
+  for (var i = 0; i < 10; i++) {
     tadpoles.push(new Tadpole(random(width), random(height), childR));
   }
+
+  thisTime = millis();
 }
 
 function draw() {
   background(255);
   var mouseVec = createVector(mouseX, mouseY);
-  
-  
-  for(var i = 0; i < tadpoles.length; i++){
-    var t = tadpoles[i];
+
+  // //var newTadpoles = tadpoles.slice();
+  // var deadTadpoles = [];
+  // //update the tadpoles
+  tadpoles.forEach(function(t) {
+  //   var dist2Mouse = mouseVec.dist(t.pos);
+  //   if (dist2Mouse < mumR / 2) {
+  //     //this t dies.
+  //     t.isDead = true;
+  //   }
+  //   if(t.isDead){deadTadpoles.push(t);}
     t.flock(tadpoles);
     t.update();
     t.checkEdges();
     t.display();
-  }
+  });
+  // console.log(deadTadpoles);
+  // var newTadpoles = tadpoles.filter(function(t){t.isDead==true});
   
-
+  
+  //draw the mouse
   if (mouseX != 0 && mouseY != 0) {
     mumPos.x = mouseX;
     mumPos.y = mouseY;
@@ -42,4 +56,31 @@ function draw() {
     mumPos.y = height / 2;
     ellipse(width / 2, height / 2, mumR, mumR);
   }
+
+
+  //draw new tabpoles
+  //set a timer, each 1 sseconds
+  if (millis() - thisTime > 200) {
+    drawTabpoles();
+    thisTime = millis();
+  }
+
+  //if the length exceeds 20, delete the old tadpoles
+  if (tadpoles.length >= 50) {
+    //tadpoles = tadpoles.reverse();
+    //tadpoles.pop();
+    tadpoles = tadpoles.slice(1);
+  }
+
+  //if it is in the black circle(mouse), it die.
+
+}
+
+function drawTabpoles() {
+  var newT = new Tadpole(random(200), random(200));
+  tadpoles.push(newT);
+}
+
+function mouseClicked() {
+  tadpoles = [];
 }
--- projects/both-sides-lab/4-tadpole/tadpole.js
+++ projects/both-sides-lab/4-tadpole/tadpole.js
@@ -7,6 +7,7 @@ class Tadpole {
       this.acc = createVector();
       this.r = 8;
       this.mass = 1;
+      this.isDead = false;
 
       this.maxSpeed = 2; // max speed;
       this.maxSteerForce = 0.05; // max steering force
@@ -69,6 +70,7 @@ class Tadpole {
     this.acc.add(force);
   }
   flock(tadpoles) {
+    //attracted by the mouse
     var mouseVec = createVector(mouseX, mouseY);
     var seekForce = this.seek(mouseVec);
     var sepaForce = this.separate(tadpoles);
